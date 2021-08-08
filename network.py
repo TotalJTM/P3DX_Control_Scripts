787636f024786a9b3ab5b3b383dfc29518686e00
Jeremy McGinnis <jermcg22@gmail.com>
2021-08-07 21:06:49 -0400

    network.py testing

--- network.py
+++ network.py
@@ -5,6 +5,8 @@
 import asyncio
 import websockets
 
+server_ip = "192.168.1.5"
+
 async def hello(websocket, path):
     name = await websocket.recv()
     print(f"< {name}")
@@ -14,7 +16,7 @@ async def hello(websocket, path):
     await websocket.send(greeting)
     print(f"> {greeting}")
 
-start_server = websockets.serve(hello, "localhost", 8765)
+start_server = websockets.serve(hello, server_ip, 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
