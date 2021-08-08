3dcc954e85567bd1a6e6885b14ad418cddf0d83f
Jeremy McGinnis <jermcg22@gmail.com>
2021-08-07 22:46:30 -0400

    even more network.py testing

--- network.py
+++ network.py
@@ -1,16 +1,19 @@
 import socket
+import sys
 
-HOST = '192.168.1.5'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+server_ip = '192.168.1.5'
+server_port = '12345'
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+messages = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), server_port))
+
+msg = s.recv(1024)
+print(msg.decode("utf-8"))
+
+sock = ""
+
+def init(server_ip, server_port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(server_ip, server_port)
